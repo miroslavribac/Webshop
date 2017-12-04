7bafe1266d458a8248ca5c9ed578f315e2ed241e
miroslavribac <miroslavribac@gmail.com>
2017-12-04 22:18:51 +0100

    Update js

--- WebshopBundle/Resources/public/js/custom.js
+++ WebshopBundle/Resources/public/js/custom.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
 
-    const height = $(window).height();
-    const scrollTop = $(window).scrollTop();
-    alert(height);
+    $(window).scroll(function(){
+        if($(this).scrollTop() < 800){
+            $(".chevron").css("display", "none");
+        } else{
+            $(".chevron").css("display", "block");
+        }
+    });
+
+    $(".chevron").click(function(e){
+        e.preventDefault();
+        var i = $(window).scrollTop();
+        var int = setInterval(function() {
+            window.scrollTo(0, i);
+            i -= 80;
+            if (i <= -111) clearInterval(int);
+        }, 40);
+    });
 
 });
