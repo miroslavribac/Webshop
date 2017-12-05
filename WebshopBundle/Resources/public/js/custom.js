05c0783add9851dabcaf2e65e408417eef75927b
miroslavribac <miroslavribac@gmail.com>
2017-12-05 09:30:26 +0100

    Update js

--- WebshopBundle/Resources/public/js/custom.js
+++ WebshopBundle/Resources/public/js/custom.js
@@ -1,21 +1,39 @@
 $(document).ready(function(){
 
-    $(window).scroll(function(){
-        if($(this).scrollTop() < 800){
-            $(".chevron").css("display", "none");
-        } else{
-            $(".chevron").css("display", "block");
-        }
-    });
+// Set css to scroll button and scroll up
+    function pageScrollUp() {
+        $(window).scroll(function(){
+            if($(this).scrollTop() < 800){
+                $(".chevron").css("display", "none");
+            } else{
+                $(".chevron").css("display", "block");
+            }
+        });
+        $(".chevron").click(function(e){
+            e.preventDefault();
+            var i = $(window).scrollTop();
+            var int = setInterval(function() {
+                window.scrollTo(0, i);
+                i -= 80;
+                if (i <= -111) clearInterval(int);
+            }, 40);
+        });
+    }
+    pageScrollUp();
 
-    $(".chevron").click(function(e){
-        e.preventDefault();
-        var i = $(window).scrollTop();
-        var int = setInterval(function() {
-            window.scrollTo(0, i);
-            i -= 80;
-            if (i <= -111) clearInterval(int);
-        }, 40);
-    });
+
+
+     // Open - close search field in header
+    function openClosesearchField() {
+        $(".header__search.search.search__open").click(function(){
+            $( "body" ).toggleClass( "search__box__show__hide");
+        });
+        $(".search__close__btn_icon").click(function(){
+            $( "body" ).toggleClass( "search__box__show__hide");
+        });
+    }
+    openClosesearchField();
+
+
 
 });
