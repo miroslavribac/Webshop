7afd551df25251a4ce1cdce7afb0bacd2d81328c
miroslavribac <miroslavribac@gmail.com>
2017-12-05 22:15:34 +0100

    Create User entity class for fos user bundle

--- WebshopBundle/Entity/User.php
+++ WebshopBundle/Entity/User.php
@@ -1,12 +1,26 @@
 <?php
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
 
 /**
- * Created by PhpStorm.
- * User: miroslav
- * Date: 5.12.17.
- * Time: 22.02
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
  */
-class User
+class User extends BaseUser
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
 }
