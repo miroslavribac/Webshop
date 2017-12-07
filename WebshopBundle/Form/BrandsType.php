cc7ba02703d8aac123b60109e4eef035771370bb
miroslavribac <miroslavribac@gmail.com>
2017-12-07 20:31:21 +0100

    Update Brands form

--- WebshopBundle/Form/BrandsType.php
+++ WebshopBundle/Form/BrandsType.php
@@ -3,6 +3,9 @@
 namespace WebshopBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,22 @@ class BrandsType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('description');
+        $builder
+            ->add('name', TextType::class,[
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                "attr" => [
+                    "class" => "btn btn-primary"
+                ]
+            ]);
     }
     
     /**
